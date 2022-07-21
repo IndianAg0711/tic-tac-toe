95f5a408264574f246d3e2020e7159ad5bacd211
IndianAg0711 <joseph.villafranca@gmail.com>
2022-07-21 13:37:27 -0500

    add board function/React component
    This will draw the board layout and handle the main game state for
    now, utilizing square component as a child

---
+++ src/Board.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import Square from './Square.js'
+
+function Board() {
+  const [xArray, setXArray] = useState([])
+  const [yArray, setYArray] = useState([])
+  const [isPlayerX, setPlayer] = useState(true)
+
+  function playerHasWon(array) {
+    if ([1, 2, 3].every(square => array.includes(square))) {
+      return true
+    }
+    if ([4, 5, 6].every(square => array.includes(square))) {
+      return true
+    }
+    if ([7, 8, 9].every(square => array.includes(square))) {
+      return true
+    }
+    if ([1, 4, 7].every(square => array.includes(square))) {
+      return true
+    }
+    if ([2, 5, 8].every(square => array.includes(square))) {
+      return true
+    }
+    if ([3, 6, 9].every(square => array.includes(square))) {
+      return true
+    }
+    if ([1, 5, 9].every(square => array.includes(square))) {
+      return true
+    }
+    if ([3, 5, 7].every(square => array.includes(square))) {
+      return true
+    }
+    return false
+  }
+
+  function handlePlayerClick(isPlayerX, square) {
+    if (isPlayerX === true) {
+      xArray.push(square)
+      setXArray(xArray)
+      if (playerHasWon(xArray)) {
+        alert('Player 1 has won!')
+      }
+    } else {
+      yArray.push(square)
+      setYArray(yArray)
+      if (playerHasWon(yArray)) {
+        alert('Player 2 has won!')
+      }
+    }
+
+    setPlayer(!isPlayerX)
+  }
+
+  function displayMarker(square) {
+    if (xArray.includes(square)) {
+      return 'X'
+    } else if (yArray.includes(square)) {
+      return 'Y'
+    } else {
+      return
+    }
+  }
+
+
+  return (
+    <div className="boardContainer">
+      <div className="board">
+        <div className="row">
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={1} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={2} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={3} />
+        </div>
+        <div className="row">
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={4} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={5} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={6} />
+        </div>
+        <div className="row">
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={7} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={8} />
+          <Square displayMarker={displayMarker} handlePlayerClick={handlePlayerClick} isPlayerX={isPlayerX} square={9} />
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export default Board
